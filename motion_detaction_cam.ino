d1d6f0687e1827560c41ef5bda9622c61527dd86
DIYQiQi <54367407+DIYQiQi@users.noreply.github.com>
2020-10-01 16:18:56 +0800

    Update motion_detaction_cam.ino

--- motion_detaction_cam.ino
+++ motion_detaction_cam.ino
@@ -1,53 +1,59 @@
-// Sweep
-// by BARRAGAN <http://barraganstudio.com> 
-// This example code is in the public domain.
-
-
 #include <Servo.h> 
- 
+
+#define MODE_PIN 8
+#define SERVO_PIN 9
+#define SENSOR_PIN 10
+#define LED 14
 Servo myservo;  // create servo object to control a servo 
                 // a maximum of eight servo objects can be created 
- 
-int LED = 14;    // led
-int sensor_pin = 8; // PIN P2.0
-int mode_pin = 10;	// PIN P2.2
+                
 int prev_state = 0;
+int time_count = 0;
 
 void setup() 
 { 
-  pinMode(sensor_pin, INPUT);
-  pinMode(mode_pin, INPUT);
+  pinMode(MODE_PIN, INPUT_PULLUP);	// PIN P2.2
+  pinMode(SENSOR_PIN, INPUT);       // sensor input pin(P2.0)
   pinMode(LED, OUTPUT);
-  myservo.attach(9);  // attaches the servo on pin 9(P2.1) to the servo object 
-  myservo.write(0);
+  myservo.attach(SERVO_PIN);        // attaches the servo on pin 9(P2.1) to the servo object
+  myservo.write(20);
 } 
+
+void click_button(){
+  myservo.write(5);
+  delay(1000);
+  myservo.write(20);
+}
 
 void loop() 
 { 
 
-  int curr_state = digitalRead(sensor_pin);
-  if (curr_state == 1) {
-    digitalWrite(LED, HIGH);
-  } else {
-    digitalWrite(LED, LOW);
-  }
-  
-  int mode = digitalRead(mode_pin);
-  if (mode == 1) {
-	  // click to start or stop recording video
-	  if (prev_state != curr_state) {
-			myservo.write(20);
-			delay(500);
-			myservo.write(5);
-	  }  
-  } else if (curr_state == 1) {
-		// click to take photo
-		myservo.write(20);
-		delay(500);
-		myservo.write(5);
-  }
-  
-  prev_state = curr_state;
-  delay(1000);
-  
+	int mode = digitalRead(MODE_PIN);
+	int curr_state = digitalRead(SENSOR_PIN);
+	if (curr_state == 1) {
+		digitalWrite(LED, HIGH);
+	} else {
+		digitalWrite(LED, LOW);
+	}
+	if (mode == 0) {
+		if (prev_state != curr_state) {
+			// click to start or stop recording video
+			click_button();
+		}
+	} else {
+		if (curr_state == 1) {
+			if (time_count == 0) {
+				click_button();		// click to take photo
+			}
+			time_count++;
+			if (time_count == 3) {
+				time_count = 0;		// take a photo every 3 seconds.
+			}
+		} else {
+			time_count = 0;
+		}
+	}
+	  
+	prev_state = curr_state;
+	 delay(1000);
 } 
